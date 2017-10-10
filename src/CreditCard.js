b786f72447f38d37a8c9eede13243e3a1072ba8b
dshaps10 <dan.shapiro1210@gmail.com>
2017-10-10 11:31:59 -0700

    Create basic CreditCard component structure

---
+++ src/CreditCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CreditCard = () => {
+    return(
+        <div>
+            <h3>Credit Card Title</h3>
+            <p>Credit Card description</p>
+        </div>
+    );
+};
+
+export default CreditCard;
