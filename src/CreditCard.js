6ed639574a76e1f52d868480ecedf20b5da665e2
dshaps10 <dan.shapiro1210@gmail.com>
2017-10-10 13:13:55 -0700

    Format card components

--- src/CreditCard.js
+++ src/CreditCard.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './CreditCard.css';
 
 class CreditCard extends Component {
     constructor(props) {
@@ -8,8 +9,30 @@ class CreditCard extends Component {
     render() {
         return(
             <div>
-                <h3>Community Bank {this.props.name}</h3>
-                <p>Credit Card description</p>
+                <hr />
+                <div className="row">
+                    <h3>Community Bank {this.props.name}</h3>
+                </div>
+                <div className="row">
+                    <div className="col-md-4">
+                        <p>This is where the picture will go</p>
+                    </div>
+                    <div className="col-md-2">
+                        <h4>Rewards</h4>
+                        <p>This is where the rewards description will go</p>
+                    </div>
+                    <div className="col-md-2">
+                        <h4>APR</h4>
+                        <p>APR%</p>
+                    </div>
+                    <div className="col-md-2">
+                        <h4>Annual Fee</h4>
+                        <p>Annual%</p>
+                    </div>
+                    <div className="col-md-2">
+                        <button type="button" class="btn btn-primary">LEARN MORE</button>
+                    </div>
+                </div>
             </div>
         );
     }
