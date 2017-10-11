00425220bf399881a6db9c2bdea4bc69ab5c45c1
dshaps10 <dan.shapiro1210@gmail.com>
2017-10-11 09:33:38 -0700

    Implement basic router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+
 import './styles/index.css';
 import App from './components/App';
+import CreditCard from './components/CreditCard';
+import CreditCardPage from './containers/CreditCardPage';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((
+    <BrowserRouter>
+        <Route path="/" component={ CreditCardPage }/>
+    </BrowserRouter>    
+), document.getElementById('root'));
+
 registerServiceWorker();
